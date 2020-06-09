3ad1630b8e82d31e2d153274e55034c54cccdd5a
Ruthie Newman <ruthienewman@protonmail.com>
2020-06-09 10:43:01 -0700

    add form div and id to html, add submit button to html, add form submit function to js, add For loop to process user input.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $("form#counter").submit(function(event){
+
+  const countto=parseInt($("#num1").val());
+  const countby=parseInt($("#num2").val()); 
+
+  for (let index= countby; index <= countto; index += countby) {
+  console.log(index);
+   // alert (index);
+  }
+
+  event.preventDefault();
+});
+});
+
