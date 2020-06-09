4a8b96b4747a87fa3c09206737dfc24bc2612bc5
Ruthie Newman <ruthienewman@protonmail.com>
2020-06-09 10:59:40 -0700

    add output function to js and ul for output in js.
    Co-authored by: Matt Macfarland<mrmcfarland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,12 @@ $(document).ready(function(){
 
   const countto=parseInt($("#num1").val());
   const countby=parseInt($("#num2").val()); 
-
+  
   for (let index= countby; index <= countto; index += countby) {
   console.log(index);
    // alert (index);
+  $("#output").append("<li>" + index + "</li>");
+
   }
 
   event.preventDefault();
